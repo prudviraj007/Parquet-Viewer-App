f51b3cc729e1a82bfbbce90cedc7bbd70417b0c3
prudviraj007 <prudvi.kilaparty@gmail.com>
2023-12-22 11:00:56 +0800

    Updated the scripts with additional features
    ++ Added Select All button
    ++ Added Progress Bar
    ++ Limited the display rows to 1000.

--- Parquet-Viewer/parquet-viewer.py
+++ Parquet-Viewer/parquet-viewer.py
@@ -1,26 +1,75 @@
 import os
 import tkinter as tk
 from tkinter import filedialog, ttk
+import dask.dataframe as dd
 import pandas as pd
-import pyarrow.parquet as pq
-import glob
 import tkinter.messagebox
 from PIL import Image, ImageTk
+from tqdm import tqdm
+from threading import Thread
+
+root = tk.Tk()
+root.geometry('800x600')
+root.title('Parquet Viewer')
+
+# Load icons using Pillow
+folder_icon = ImageTk.PhotoImage(Image.open("folder_icon.png").resize((24, 24)))
+file_icon = ImageTk.PhotoImage(Image.open("file_icon.jpeg").resize((24, 24)))
+copy_icon = ImageTk.PhotoImage(Image.open("copy_icon.jpeg").resize((24, 24)))
+select_all_icon = ImageTk.PhotoImage(Image.open("selectall_icon.png").resize((24, 24)))
 
 def open_folder():
     folder_path = filedialog.askdirectory()
     if folder_path:
-        parquet_files = glob.glob(os.path.join(folder_path, '*.parquet'))
-        data = pd.concat((pd.read_parquet(file) for file in parquet_files))
-        update_tree(data)
-        update_info(folder_path, data)
+        folder_files = os.listdir(folder_path)
+        parquet_files = [os.path.join(folder_path, file) for file in folder_files if file.endswith('.parquet') and os.path.isfile(os.path.join(folder_path, file))]
+
+        if not parquet_files:
+            tkinter.messagebox.showinfo("No Parquet Files", "No Parquet files found in the selected folder.")
+            return
+
+        try:
+            data_frames = []
+
+            def load_files():
+                for file in tqdm(parquet_files, desc="Loading Files", unit="file"):
+                    try:
+                        dask_data = dd.read_parquet(file, engine='pyarrow')
+                        df = dask_data.compute()
+                        data_frames.append(df)
+                    except Exception as e:
+                        tkinter.messagebox.showwarning("Error Reading File", f"Error reading file '{file}': {str(e)}")
+
+                progress_bar.stop()
+                progress_bar.destroy()
+
+                data = pd.concat(data_frames)
+                update_tree(data)
+                update_info(folder_path, data)
+
+            progress_bar = ttk.Progressbar(root, mode='indeterminate')
+            progress_bar.grid(row=2, column=0, columnspan=4, pady=5, sticky='ew')
+            progress_bar.start()
+
+            # Run loading files in a separate thread
+            Thread(target=load_files).start()
+
+        except Exception as e:
+            tkinter.messagebox.showwarning("Error Reading Files", f"Error reading files: {str(e)}")
 
 def open_file():
     file_path = filedialog.askopenfilename(filetypes=[("Parquet files", "*.parquet")])
     if file_path:
-        data = pd.read_parquet(file_path)
-        update_tree(data)
-        update_info(os.path.dirname(file_path), data)
+        try:
+            data = pd.read_parquet(file_path)
+            update_tree(data)
+            update_info(os.path.dirname(file_path), data)
+        except Exception as e:
+            tkinter.messagebox.showwarning("Error Reading File", f"Error reading file '{file_path}': {str(e)}")
+
+def select_all_rows():
+    items = tree.get_children()
+    tree.selection_set(items)
 
 def copy_to_clipboard():
     selected_items = tree.selection()
@@ -44,7 +93,10 @@ def update_tree(data):
     tree["show"] = "headings"
     for column in tree["columns"]:
         tree.heading(column, text=column)
-    df_rows = data.to_numpy().tolist()
+
+    # Display the first 1000 rows
+    df_rows = data.head(1000).to_numpy().tolist()
+    
     for row in df_rows:
         tree.insert("", "end", values=row)
 
@@ -56,28 +108,22 @@ def update_info(folder_path, data):
     for column in data.columns:
         info.insert(tk.END, f'  {column}\n')
 
-root = tk.Tk()
-root.geometry('800x600')
-root.title('Parquet Viewer')
-
-# Load icons using Pillow
-folder_icon = ImageTk.PhotoImage(Image.open("folder_icon.png").resize((24, 24)))
-file_icon = ImageTk.PhotoImage(Image.open("file_icon.jpeg").resize((24, 24)))
-copy_icon = ImageTk.PhotoImage(Image.open("copy_icon.jpeg").resize((24, 24)))
-
 # Frame for top row buttons
-top_frame = tk.Frame(root)
-top_frame.grid(row=0, column=0, columnspan=2, pady=5)
+top_button_frame = ttk.Frame(root)
+top_button_frame.grid(row=0, column=0, columnspan=4, pady=5)
 
 # Top row buttons
-open_folder_button = tk.Button(top_frame, text="Open Folder", image=folder_icon, compound="top", command=open_folder)
+open_folder_button = tk.Button(top_button_frame, text="Open Folder", image=folder_icon, compound="top", command=open_folder)
 open_folder_button.grid(row=0, column=0, padx=5)
 
-open_file_button = tk.Button(top_frame, text="Open File", image=file_icon, compound="top", command=open_file)
+open_file_button = tk.Button(top_button_frame, text="Open File", image=file_icon, compound="top", command=open_file)
 open_file_button.grid(row=0, column=1, padx=5)
 
-copy_button = tk.Button(top_frame, text="Copy to Clipboard", image=copy_icon, compound="top", command=copy_to_clipboard)
-copy_button.grid(row=0, column=2, padx=5)
+select_all_button = tk.Button(top_button_frame, text="Select All", image=select_all_icon, compound="top", command=select_all_rows)
+select_all_button.grid(row=0, column=2, padx=5)
+
+copy_button = tk.Button(top_button_frame, text="Copy to Clipboard", image=copy_icon, compound="top", command=copy_to_clipboard)
+copy_button.grid(row=0, column=3, padx=5)
 
 # Information frame
 info_frame = tk.Frame(root)
